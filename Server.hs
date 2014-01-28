429a9198dc58c93ef288c7284dd42496ed364fda
Stefan Bleibinhaus <stefan.bleibinhaus@gmail.com>
2014-01-28 18:53:29 +1300

    Adds a file path to write the port to. Fixes format for apache.

--- Server.hs
+++ Server.hs
@@ -7,9 +7,12 @@ import System.IO.Unsafe
 port :: Int
 port = unsafePerformIO (getStdRandom (randomR (3000, 3010)))
 
+filePath :: String
+filePath = "/etc/apache2/server-port"
+
 main = do {
   -- print $ "The server is running on port " ++ show port;
-  writeFile "server-port" (show port);
+  writeFile filePath ("port " ++ show port);
   scotty port $ do
     get "/" $ do
       text "Ok"
